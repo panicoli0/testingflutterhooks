c10e3e7380064d161e7111e11c5a3a3f37d43bb2
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-01-10 10:25:06 +0400

    using future hook to cache img.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(
@@ -14,27 +26,20 @@ void main() {
   );
 }
 
-// Stream<String> getTime() => Stream.periodic(
-//       const Duration(seconds: 1),
-//       (_) => DateTime.now().toIso8601String(),
-//     );
+const _url =
+    'https://www.timeoutdubai.com/cloud/timeoutdubai/2022/02/04/Museum-of-the-Future.jpg';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final _controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        _controller.addListener(() {
-          text.value = _controller.text;
-        });
-        return null;
-      },
-      [_controller],
-    );
+    final future = useMemoized(() => (NetworkAssetBundle(Uri.parse(_url))
+        .load(_url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data))));
+
+    final snapshot = useFuture(future);
 
     return Scaffold(
       appBar: AppBar(
@@ -43,14 +48,7 @@ class HomePage extends HookWidget {
         ),
       ),
       body: Column(
-        children: [
-          TextField(
-            controller: _controller,
-          ),
-          Text(
-            'You typed: ${text.value}',
-          ),
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
